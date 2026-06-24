91fd8126b9fb039d39d3afdd232149e51408e896
LeSalty1 <endogenousvar@gmail.com>
2026-06-24 15:09:31 -0400

    Add sublist function for list comparison
    Implement sublist function to compare two lists.

---
+++ Sublist.py
@@ -0,0 +1,17 @@
+# This program takes two lists and checks if one is a sub/superlist of the other.
+SUBLIST = 1
+SUPERLIST = 2
+EQUAL = 3
+UNEQUAL = 4
+def sublist(list_one, list_two):
+  s_list_one = str(list_one).strip("[]") + ","
+  s_list_two = str(list_two).strip("[]") + ","
+  if s_list_one == s_list_two: 
+    return EQUAL 
+  elif s_list_one in s_list_two: 
+    return SUBLIST
+  elif s_list_two in s_list_one: 
+    return SUPERLIST 
+  return UNEQUAL 
+
+            
