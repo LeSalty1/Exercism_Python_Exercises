7788ae889d66c6a6edf9f826d210a012da369077
SaltyArjun <endogenousvar@gmail.com>
2026-07-05 15:53:06 -0400

    Refactor to follow Python code style convention.

--- Rebase.py
+++ Rebase.py
@@ -4,15 +4,14 @@ def rebase(input_base, digits, output_base):
     place = 0
     if input_base < 2: 
         raise ValueError("input base must be >= 2")
-    elif output_base < 2: 
+    if output_base < 2: 
         raise ValueError("output base must be >= 2")
-    else: 
-        for digit in digits[-1::-1]: # Converts input_base, digits -> base-10
-            if 0 <= digit < input_base: 
-                tot += digit*(input_base ** place) 
-                place += 1
-            else: 
-                raise ValueError("all digits must satisfy 0 <= d < input base")
+    for digit in digits[-1::-1]: # Converts input_base, digits -> base-10
+        if 0 <= digit < input_base: 
+            tot += digit*(input_base ** place) 
+            place += 1
+        else: 
+            raise ValueError("all digits must satisfy 0 <= d < input base")
     # need to convert base-10 -> output_base, [digits]
     output_list = []
     while tot > 0: 
