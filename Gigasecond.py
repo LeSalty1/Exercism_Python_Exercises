176339e289f4e0be5bb631b250db0fdcbda01af3
SaltyArjun <endogenousvar@gmail.com>
2026-07-06 11:20:22 -0400

    Update Gigasecond.py

--- Gigasecond.py
+++ Gigasecond.py
@@ -1,4 +1,5 @@
 # This code takes a date and returns the date after one gigasecond.
+# This can easily be generalized for any length of time.
 import datetime as dt
 def add(moment):
     return moment + dt.timedelta(seconds = 1e9)
